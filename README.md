096d01acad02fe7862136ffd739391c21e4a6c75
aresy.z <yzsolo125@gmail.com>
2018-08-02 19:14:34 +0800

    dev

---
+++ README.md
@@ -0,0 +1,7 @@
+# transkit
+自用命令行翻译工具
+
+### Installation
+```
+npm install --save transkit
+```
---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+ 
+let program = require('commander');
+let http = require('http');
+let querystring = require('querystring');
+let crypto=require('crypto');
+
+let appId = '03c86605ee56de31';
+let appKey = 'ASnBvRUIZW8YQoVFQD4hyP6TOLrn62KK';
+let cmdValue = undefined;
+
+let getMd5 = (key) => {
+	let md5=crypto.createHash("md5");
+	md5.update(key);
+	let str=md5.digest('hex');
+	let s=str.toUpperCase();  
+	return s;
+}
+
+let getSalt = () => {
+	return Math.round(Math.random()*100000000000);
+}
+
+let trans = (q) => {
+	let salt = getSalt();
+	let contents = querystring.stringify({
+	    q: q,
+	    from: 'auto',
+	    to: 'auto',
+	    appKey: appId,
+	    salt: salt,
+	    sign: getMd5(appId + q + salt + appKey),
+	});
+
+	let options = {
+	    host:'openapi.youdao.com',
+	    path:'/api',
+	    method:'POST',
+	    headers:{
+	        'Content-Type':'application/x-www-form-urlencoded',
+	        'Content-Length':contents.length
+	    }
+	}
+
+    let req = http.request(options, function(res){
+	    res.setEncoding('utf8');
+	    res.on('data',function(data){
+	    	let datas = JSON.parse(data);
+	    	let phonetic = datas.basic.phonetic ? "["+datas.basic.phonetic+"]" : '';
+	        console.log(datas.query + phonetic);
+	        console.log('翻译：');
+	        datas.basic.explains.map((item)=>console.log('  ' + item));
+	        
+	    });
+	});
+    req.write(contents);
+	req.end();
+}
+ 
+program
+  .version('0.0.8')
+  .arguments('<cmd>')
+  .action((cmd)=>{
+  	cmdValue = cmd;
+  });
+
+program.parse(process.argv);
+
+if(cmdValue === undefined) {
+	console.error('input the word you want to trans');
+	process.exit(1);
+} else {
+	trans(cmdValue);
+}
